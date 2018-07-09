1d2ea373907adbf4d40c86e3fd8e3c23577a415f
masontang@aol.com <masontang@aol.com>
2018-07-09 15:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ function nestedTarget(){
   return document.querySelector("#nested .target")
 }
 
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
 
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
 
 
 function deepestChild() {
